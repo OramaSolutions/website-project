4730d025cb512d9cd44bc62cd3e727fc89bd7ad9
adyeetya <2612adityasingh2000@gmail.com>
2026-02-17 17:06:43 +0530

    enq form with erp and backend

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-// export const Server_Url = "https://nocode-node.oramasolutions.in/api/v1";
-export const Server_Url = "http://localhost:3100/api/v1";
+export const Server_Url = "https://nocode-node.oramasolutions.in/api/v1";
+// export const Server_Url = "http://localhost:3100/api/v1";
 
 
 export const SERVER_URL = Server_Url;
